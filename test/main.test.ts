3852ea37c1119a42824a145c28a9070c34419acb
Jonathan Lloyd <webmaster@lifegames.org>
2019-11-25 08:37:14 -0800

    Updates to the test harness to support code coverage with typescript -- not yet working

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,12 +1,13 @@
+import {CustomAuthorizerEvent} from 'aws-lambda'
 import * as AWS from 'aws-sdk'
 import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import {expect} from 'chai'
 import * as crypto from 'crypto'
-import * as fs from 'fs'
 import * as sinon from 'sinon'
-import {uploadFilePart} from '../dist/main'
-import {UploadPartEvent} from '../src/types/main'
+import {handleAuthorization, uploadFilePart} from '../src/main'
+import {CompleteFileUploadEvent, UploadPartEvent} from '../src/types/main'
+import * as ApiGateway from './../dist/lib/vendor/AWS/ApiGateway'
 import * as S3 from './../dist/lib/vendor/AWS/S3'
 
 const bucket = 'lifegames-fileviewer-s3bucket-yqm2cswg5ozl'
@@ -23,6 +24,46 @@ sinon.stub(S3, 'uploadPart').returns({
     ETag: crypto.createHash('md5').update('some_string').digest('hex')
 })
 
+sinon.stub(ApiGateway, 'getApiKeys').returns({
+    items: [
+        {
+            createdDate: '2019-07-23T17:24:49.000Z',
+            description: 'The key for the iOS App',
+            enabled: true,
+            id: 'yzr34xwmb0',
+            lastUpdatedDate: '2019-07-23T17:24:49.000Z',
+            name: 'iOSAppKey',
+            stageKeys: [],
+            value: 'pRauC0NteI2XM5zSLgDzDaROosvnk1kF1H0ID2zc'
+        }
+    ]
+})
+
+sinon.stub(ApiGateway, 'getUsagePlans').returns({
+    items: [
+        {
+            apiStages: [
+                {
+                    apiId: 'zc21p8daqc',
+                    stage: 'Prod'
+                }
+            ],
+            description: 'Internal consumption',
+            id: 'fujp25',
+            name: 'Basic'
+        }
+    ]
+})
+
+sinon.stub(ApiGateway, 'getUsage').returns({
+    endDate: '2019-11-23',
+    items: {
+        yzr34xwmb0: [[2, 0]]
+    },
+    startDate: '2019-11-23',
+    usagePlanId: 'fujp25'
+})
+
 function mockResponseUploadPart(config, bytesTotal) {
     return new Promise<any[]>((resolve, reject) => {
         const [, beg, end] = /bytes=(\d+)\-(\d+)/.exec(config.headers['Range'])
@@ -35,39 +76,96 @@ function mockResponseUploadPart(config, bytesTotal) {
     })
 }
 
-describe('main', () => {
-    before(() => {
-        /*
-        sinon.stub(console, 'log')
-        sinon.stub(console, 'info')
-        sinon.stub(console, 'debug')
-        sinon.stub(console, 'warn')
-        sinon.stub(console, 'error')
-        */
+async function mockIterationsOfUploadPart(bytesTotal) {
+    return new Promise<any[]>(async (resolve, reject) => {
+        const responses = []
+        const partTags = []
+        let partNumber = 1
+        let bytesRemaining = bytesTotal
+        let partEnd = Math.min(partSize, bytesTotal) - 1
+        let partBeg = 0
+        while (bytesRemaining > 0) {
+            // tslint:disable-next-line:max-line-length
+            const event: UploadPartEvent = {bucket, bytesRemaining, bytesTotal, key, partBeg, partEnd, partNumber, partSize, partTags, uploadId, url}
+            const output: CompleteFileUploadEvent|UploadPartEvent = await uploadFilePart(event)
+            responses.push(output)
+            if (output.bytesRemaining > 0) {
+                // @ts-ignore
+                ({partBeg, partEnd, partNumber} = output)
+            }
+            bytesRemaining = bytesRemaining - partSize
+        }
+        resolve(responses)
     })
+}
+
+/*
+const response = {
+            context: {},
+            policyDocument: {
+                Statement: [{
+                        Action: 'execute-api:Invoke',
+                        Effect: 'Allow',
+                        Resource: 'arn:aws:execute-api:us-west-2:203465012143:zc21p8daqc/Prod/POST/feedly'
+                }],
+                Version: '2012-10-17'
+            },
+            principalId: 'me',
+            usageIdentifierKey: 'pRauC0NteI2XM5zSLgDzDaROosvnk1kF1H0ID2zc'
+        }
+ */
+describe('main', () => {
+    beforeEach(() => {
+        /*this.consoleLogStub = sinon.stub(console, 'log')
+        this.consoleInfoStub = sinon.stub(console, 'info')
+        this.consoleDebugStub = sinon.stub(console, 'debug')
+        this.consoleWarnStub = sinon.stub(console, 'warn')
+        this.consoleErrorStub = sinon.stub(console, 'error')*/
+    })
+    afterEach(() => {
+        /*this.consoleLogStub.restore()
+        this.consoleInfoStub.restore()
+        this.consoleDebugStub.restore()
+        this.consoleWarnStub.restore()
+        this.consoleErrorStub.restore()*/
+    })
+    describe('#handleAuthorization', () => {
+        it('should accept a valid request', async () => {
+            const apiKey = 'pRauC0NteI2XM5zSLgDzDaROosvnk1kF1H0ID2zc'
+            const event: CustomAuthorizerEvent = {
+                methodArn: 'string',
+                queryStringParameters: {ApiKey: apiKey},
+                type: 'test'
+            }
+            const output = await handleAuthorization(event)
+            expect(output.principalId).to.equal('me')
+            // @ts-ignore
+            expect(output.policyDocument.Statement[0].Effect).to.equal('Allow')
+            expect(output.usageIdentifierKey).to.equal(apiKey)
+        })
+        it('should deny an invalid ApiKey', async () => {
+            const apiKey = '1234'
+            const event: CustomAuthorizerEvent = {
+                methodArn: 'string',
+                queryStringParameters: {ApiKey: apiKey},
+                type: 'test'
+            }
+            const output = await handleAuthorization(event)
+            expect(output.principalId).to.equal('me')
+            // @ts-ignore
+            expect(output.policyDocument.Statement[0].Effect).to.equal('Deny')
+        })
+    })
+    /*
     describe('#uploadPart', () => {
+        afterEach(() => {
+            mock.resetHandlers()
+        })
         it('should handle a multipart file', async () => {
             const bytesTotal = 82784319
             mock.onAny().reply((config) => mockResponseUploadPart(config, bytesTotal))
-
             const totalParts = Math.round(bytesTotal / partSize)
-            const partTags = []
-            let partNumber = 1
-            let bytesRemaining = bytesTotal
-            let partEnd = partSize - 1
-            let partBeg = 0
-            const responses = []
-            while (bytesRemaining > 0) {
-                // tslint:disable-next-line:max-line-length
-                const event: UploadPartEvent = {bucket, bytesRemaining, bytesTotal, key, partBeg, partEnd, partNumber, partSize, partTags, uploadId, url}
-                const response = await uploadFilePart(event)
-                responses.push(response)
-                if (response.bytesRemaining > 0) {
-                    ({partBeg, partEnd, partNumber} = response)
-                }
-                bytesRemaining = response.bytesRemaining
-            }
-
+            const responses = await mockIterationsOfUploadPart(bytesTotal)
             const finalPart = responses.pop()
             const uploadPart = responses.pop()
             expect(uploadPart.partNumber).to.equal(totalParts)
@@ -75,5 +173,16 @@ describe('main', () => {
             expect(finalPart.bytesRemaining).to.equal(0)
 
         })
+        it('should handle a single part file', async () => {
+            const bytesTotal = 5242880 - 1000
+            mock.onAny().reply((config) => mockResponseUploadPart(config, bytesTotal))
+            const totalParts = Math.round(bytesTotal / partSize)
+            const responses = await mockIterationsOfUploadPart(bytesTotal)
+            expect(responses.length).to.equal(totalParts)
+            const finalPart = responses.pop()
+            expect(finalPart.partTags.length).to.equal(totalParts)
+            expect(finalPart.bytesRemaining).to.equal(0)
+        })
     })
+    */
 })
