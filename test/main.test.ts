8dd70e5cec31c8ed9c051c1f9e493177e53a0f63
Jonathan Lloyd <jonathan@turo.com>
2019-11-28 11:31:00 -0800

    Added addition test case for handleAuthentication and added test fixtures

--- test/main.test.ts
+++ test/main.test.ts
@@ -4,11 +4,12 @@ import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import {expect} from 'chai'
 import * as crypto from 'crypto'
+import * as fs from 'fs'
 import * as sinon from 'sinon'
 import {handleAuthorization, uploadFilePart} from '../src/main'
 import {CompleteFileUploadEvent, UploadPartEvent} from '../src/types/main'
-import * as ApiGateway from './../dist/lib/vendor/AWS/ApiGateway'
-import * as S3 from './../dist/lib/vendor/AWS/S3'
+import * as ApiGateway from './../src/lib/vendor/AWS/ApiGateway'
+import * as S3 from './../src/lib/vendor/AWS/S3'
 
 const bucket = 'lifegames-fileviewer-s3bucket-yqm2cswg5ozl'
 const key = '20191107-[sxephil]-Title'
@@ -24,45 +25,10 @@ sinon.stub(S3, 'uploadPart').returns({
     ETag: crypto.createHash('md5').update('some_string').digest('hex')
 })
 
-sinon.stub(ApiGateway, 'getApiKeys').returns({
-    items: [
-        {
-            createdDate: '2019-07-23T17:24:49.000Z',
-            description: 'The key for the iOS App',
-            enabled: true,
-            id: 'yzr34xwmb0',
-            lastUpdatedDate: '2019-07-23T17:24:49.000Z',
-            name: 'iOSAppKey',
-            stageKeys: [],
-            value: 'pRauC0NteI2XM5zSLgDzDaROosvnk1kF1H0ID2zc'
-        }
-    ]
-})
-
-sinon.stub(ApiGateway, 'getUsagePlans').returns({
-    items: [
-        {
-            apiStages: [
-                {
-                    apiId: 'zc21p8daqc',
-                    stage: 'Prod'
-                }
-            ],
-            description: 'Internal consumption',
-            id: 'fujp25',
-            name: 'Basic'
-        }
-    ]
-})
-
-sinon.stub(ApiGateway, 'getUsage').returns({
-    endDate: '2019-11-23',
-    items: {
-        yzr34xwmb0: [[2, 0]]
-    },
-    startDate: '2019-11-23',
-    usagePlanId: 'fujp25'
-})
+function getFixture(file) {
+    const fixturePath = './test/fixtures'
+    return JSON.parse(fs.readFileSync(`${fixturePath}/${file}`, 'utf8'))
+}
 
 function mockResponseUploadPart(config, bytesTotal) {
     return new Promise<any[]>((resolve, reject) => {
@@ -130,6 +96,16 @@ describe('main', () => {
         this.consoleErrorStub.restore()*/
     })
     describe('#handleAuthorization', () => {
+        beforeEach(() => {
+            this.getApiKeyStub = sinon.stub(ApiGateway, 'getApiKeys').returns(getFixture('getApiKeys-200-OK.json'))
+            this.getUsagePlansStub = sinon.stub(ApiGateway, 'getUsagePlans').returns(getFixture('getUsagePlans-200-OK.json'))
+            this.getUsageStub = sinon.stub(ApiGateway, 'getUsage').returns(getFixture('getUsage-200-OK.json'))
+        })
+        afterEach(() => {
+            this.getApiKeyStub.restore()
+            this.getUsagePlansStub.restore()
+            this.getUsageStub.restore()
+        })
         it('should accept a valid request', async () => {
             const apiKey = 'pRauC0NteI2XM5zSLgDzDaROosvnk1kF1H0ID2zc'
             const event: CustomAuthorizerEvent = {
@@ -154,6 +130,9 @@ describe('main', () => {
             expect(output.principalId).to.equal('me')
             // @ts-ignore
             expect(output.policyDocument.Statement[0].Effect).to.equal('Deny')
+        })
+        it('should deny a disabled ApiKey', async () => {
+            // this.getApiKeyStub.value(getFixture('getApiKeys-400-DisabledKey.json'))
         })
     })
     /*
