c46bb14217401ec9885cbf2d864ff5d2738ce095
Jonathan Lloyd <webmaster@lifegames.org>
2019-11-28 15:21:19 -0800

    Moved helper functions to a separate file

--- test/main.test.ts
+++ test/main.test.ts
@@ -2,54 +2,12 @@ import axios from 'axios'
 import MockAdapter from 'axios-mock-adapter'
 import {expect} from 'chai'
 import crypto from 'crypto'
-import * as fs from 'fs'
 import * as sinon from 'sinon'
-import {handleAuthorization, uploadFilePart} from '../src/main'
+import {handleAuthorization} from '../src/main'
 import * as ApiGateway from './../src/lib/vendor/AWS/ApiGateway'
 import * as S3 from './../src/lib/vendor/AWS/S3'
+import {getFixture, mockIterationsOfUploadPart, mockResponseUploadPart} from './helper'
 
-function getFixture(file) {
-    const fixturePath = './test/fixtures'
-    return JSON.parse(fs.readFileSync(`${fixturePath}/${file}`, 'utf8'))
-}
-
-function mockResponseUploadPart(config, bytesTotal, partSize) {
-    return new Promise<any[]>((resolve, reject) => {
-        const [, beg, end] = /bytes=(\d+)\-(\d+)/.exec(config.headers.Range)
-        return resolve([206, 'hello', {
-            'accept-ranges': 'bytes',
-            'content-length': partSize,
-            'content-range': `bytes ${beg}-${end}/${bytesTotal}`,
-            'content-type': 'video/mp4'
-        }])
-    })
-}
-
-function mockIterationsOfUploadPart(bytesTotal, partSize) {
-    const bucket = 'lifegames-fileviewer-s3bucket-yqm2cswg5ozl'
-    const key = '20191107-[sxephil]-Title'
-    const uploadId = 'some-id1'
-    const url = 'https://example.com/some-video.mp4'
-    return new Promise<any[]>(async (resolve, reject) => {
-        const responses = []
-        const partTags = []
-        let partNumber = 1
-        let bytesRemaining = bytesTotal
-        let partEnd = Math.min(partSize, bytesTotal) - 1
-        let partBeg = 0
-        while (bytesRemaining > 0) {
-            const event = {bucket, bytesRemaining, bytesTotal, key, partBeg, partEnd, partNumber, partSize, partTags, uploadId, url}
-            const output = await uploadFilePart(event)
-            responses.push(output)
-            if (output.bytesRemaining > 0) {
-                // @ts-ignore
-                ({partBeg, partEnd, partNumber} = output)
-            }
-            bytesRemaining = bytesRemaining - partSize
-        }
-        resolve(responses)
-    })
-}
 describe('main', () => {
     beforeEach(() => {
         this.consoleLogStub = sinon.stub(console, 'log')
