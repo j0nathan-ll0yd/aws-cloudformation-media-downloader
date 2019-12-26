d8e207d85dad9b68719ee5531d381fc6738bc62e
Jonathan Lloyd <webmaster@lifegames.org>
2019-12-26 09:56:40 -0800

    Added a method for testing registration for push notifications on iOS

--- bin/test-registerDevice.sh
+++ bin/test-registerDevice.sh
@@ -5,11 +5,11 @@ bin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source "${bin_dir}/test-setup.sh"
 
-# TODO: Allow for a YouTube URL to be submitted
+REQUEST_DATA=`cat test/fixtures/handleRegisterDevice/APIGatewayEvent.json | jq -r '.body'`
 REQUEST_URL="https://${SUBDOMAIN}.execute-api.${AWS_REGION}.amazonaws.com/${STAGE}/registerDevice?ApiKey=${API_KEY}"
 echo "Calling ${REQUEST_URL}"
 curl -X POST -v -H "Content-Type: application/json" \
 -H "Accept: application/json" \
---data @./test/fixtures/handleRegisterDevice/body.json \
+--data "${REQUEST_DATA}" \
 $REQUEST_URL | jsonpp
 
