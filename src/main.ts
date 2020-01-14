341642b0619037922e42ede6c9ecee78e0a4afd4
Jonathan Lloyd <jonathan@turo.com>
2020-01-13 17:10:15 -0800

    Refactored console logging; added consistency to error messages

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ import {ScheduledEvent} from './types/vendor/Amazon/CloudWatch/ScheduledEvent'
 import {Webhook} from './types/vendor/IFTTT/Feedly/Webhook'
 import {generateAllow, generateDeny} from './util/apigateway-helpers'
 import {feedlyEventConstraints, registerDeviceConstraints} from './util/constraints'
-import {response} from './util/lambda-helpers'
+import {logDebug, logError, logInfo, response} from './util/lambda-helpers'
 import {objectKeysToLowerCase, transformVideoInfoToMetadata, transformVideoIntoS3File} from './util/transformers'
 
 function processEventAndValidate(event: APIGatewayEvent | ScheduledEvent, constraints?) {
@@ -30,15 +30,16 @@ function processEventAndValidate(event: APIGatewayEvent | ScheduledEvent, constr
   } else if ('body' in event) {
     try {
       requestBody = JSON.parse(event.body)
-      console.log('event.body', JSON.stringify(requestBody, null, 2))
+      logDebug('processEventAndValidate.event.body <=', requestBody)
     } catch (error) {
-      console.error('event.body', `Invalid JSON: ${error}`)
+      logError('processEventAndValidate =>', `Invalid JSON: ${error}`)
       return {statusCode: 400, message: 'Request body must be valid JSON'}
     }
   }
   if (constraints) {
     const invalidAttributes = validate(requestBody, constraints)
     if (invalidAttributes) {
+      logError('processEventAndValidate =>   ', invalidAttributes)
       return {statusCode: 400, message: invalidAttributes}
     }
   }
@@ -46,38 +47,29 @@ function processEventAndValidate(event: APIGatewayEvent | ScheduledEvent, constr
 }
 
 export async function handleAuthorization(event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> {
-  console.info('Received event: ', JSON.stringify(event, null, 2))
-  // console.info('Received EV:', JSON.stringify(process.env, null, 2))
+  logInfo('event <=', event)
+  const responseDeny = generateDeny('me', event.methodArn)
   try {
     const queryStringParameters = event.queryStringParameters
-    // This should always be defined, because it's governed by the API Gateway
     const apiKeyValue = queryStringParameters.ApiKey
-    const apiKeyResponse = await getApiKeys({includeValues: true})
-    console.log('API Key data', JSON.stringify(apiKeyResponse, null, 2))
+    const getApiKeysParams = {includeValues: true}
+    logDebug('getApiKeys <=', getApiKeysParams)
+    const apiKeyResponse = await getApiKeys(getApiKeysParams)
+    logDebug('getApiKeys =>', apiKeyResponse)
     const matchedApiKeys = apiKeyResponse.items.filter((item) => item.value === apiKeyValue)
-    console.log('Matched API Key data', JSON.stringify(matchedApiKeys, null, 2))
-    console.log('Matched API Key data', matchedApiKeys.length)
     if (matchedApiKeys.length > 0) {
       const apiKey = matchedApiKeys[0]
       if (apiKey.enabled === false) {
         return generateDeny('me', event.methodArn)
       }
-      console.log('Getting usage plans')
-      const usagePlansResponse = await getUsagePlans({keyId: apiKey.id})
-      console.log('Usage plans', JSON.stringify(usagePlansResponse, null, 2))
-      let responseObject = generateAllow('me', event.methodArn)
+      const getUsagePlansParams = {keyId: apiKey.id}
+      logDebug('getUsagePlans <=', getUsagePlansParams)
+      const usagePlansResponse = await getUsagePlans(getUsagePlansParams)
+      logDebug('getUsagePlans =>', usagePlansResponse)
+      let responseAllow = generateAllow('me', event.methodArn)
       if (usagePlansResponse.items) {
         const usagePlanId = usagePlansResponse.items[0].id
-        // MyUsagePlan: k9i2ri
-        // iOSApiKey: tfmmf65cag
-        // MyUsagePlanKey: tfmmf65cag:k9i2ri
-        const usageIdentifierKey = `${apiKey.id}:${usagePlansResponse.items[0].id}`
-        console.log(`usageIdentifierKey = ${usageIdentifierKey}`)
-        // does NOT work: apiKeyValue ? Testing now
-        // does NOT work: apiKey.id (aka keyId)
-        // does NOT work: usagePlanId
-        // TO TEST: usageIdentifierKey
-        responseObject = generateAllow('me', event.methodArn, apiKeyValue)
+        responseAllow = generateAllow('me', event.methodArn, apiKeyValue)
         const usageDate = (new Date()).toISOString().split('T')[0]
         const params = {
           endDate: usageDate,
@@ -85,24 +77,23 @@ export async function handleAuthorization(event: CustomAuthorizerEvent): Promise
           startDate: usageDate,
           usagePlanId
         }
-        console.log('Getting usage data with params ', JSON.stringify(params, null, 2))
+        logDebug('getUsage <=', params)
         const usageResponse = await getUsage(params)
-        console.log('Usage data', JSON.stringify(usageResponse, null, 2))
+        logDebug('getUsage =>', usageResponse)
       }
-      console.log('Responding with ', JSON.stringify(responseObject, null, 2))
-      return responseObject
+      logInfo('response =>', responseAllow)
+      return responseAllow
     }
-    console.log('Responding with ', JSON.stringify(generateDeny('me', event.methodArn), null, 2))
-    return generateDeny('me', event.methodArn)
+    logInfo('response =>', responseDeny)
+    return responseDeny
   } catch (error) {
-    console.error(error)
-    return generateDeny('me', event.methodArn)
+    logError('response =>', responseDeny)
+    return responseDeny
   }
 }
 
 export async function handleFeedlyEvent(event: APIGatewayEvent | ScheduledEvent, context: Context) {
-  console.log('handleFeedlyEvent.event <=', JSON.stringify(event, null, 2))
-  console.log('handleFeedlyEvent.context <=', JSON.stringify(context, null, 2))
+  logInfo('event <=', event)
   const {requestBody, statusCode, message} = processEventAndValidate(event, feedlyEventConstraints)
   if (statusCode && message) {
     return response(context, statusCode, message)
@@ -114,19 +105,18 @@ export async function handleFeedlyEvent(event: APIGatewayEvent | ScheduledEvent,
       name: (new Date()).getTime().toString(),
       stateMachineArn: process.env.StateMachineArn
     }
-    console.log('startExecution <=', JSON.stringify(params, null, 2))
+    logDebug('startExecution <=', params)
     const data = await startExecution(params)
-    console.log('startExecution =>', JSON.stringify(data, null, 2))
+    logDebug('startExecution =>', data)
     return response(context, 202, {status: 'ExecutionStarted'})
   } catch (error) {
-    console.error(error)
+    logError('response =>', error)
     return response(context, 500, error.message)
   }
 }
 
 export async function handleDeviceRegistration(event: APIGatewayEvent, context: Context) {
-  console.log('handleDeviceRegistration.event <=', JSON.stringify(event, null, 2))
-  console.log('handleDeviceRegistration.context <=', JSON.stringify(context, null, 2))
+  logInfo('event <=', event)
   const {requestBody, statusCode, message} = processEventAndValidate(event, registerDeviceConstraints)
   if (statusCode && message) {
     return response(context, statusCode, message)
@@ -137,24 +127,23 @@ export async function handleDeviceRegistration(event: APIGatewayEvent, context: 
     PlatformApplicationArn: process.env.PlatformApplicationArn,
     Token: body.token
   }
-  console.log('createPlatformEndpoint <=', JSON.stringify(params, null, 2))
+  logDebug('createPlatformEndpoint <=', params)
   const data = await createPlatformEndpoint(params)
-  console.log('createPlatformEndpoint =>', JSON.stringify(data, null, 2))
+  logDebug('createPlatformEndpoint =>', data)
   return response(context, 201, {endpointArn: data.EndpointArn})
 }
 
 export async function listFiles(event: APIGatewayEvent | ScheduledEvent, context: Context) {
-  console.log('listFiles.event', JSON.stringify(event, null, 2))
-  console.log('listFiles.context', JSON.stringify(context, null, 2))
+  logInfo('event <=', event)
   const {statusCode, message} = processEventAndValidate(event)
   if (statusCode && message) {
     return response(context, statusCode, message)
   }
   try {
     const params = {Bucket: process.env.Bucket, MaxKeys: 1000}
-    console.log('listObjects <=', JSON.stringify(params, null, 2))
+    logDebug('listObjects <=', params)
     const files = await listObjects({Bucket: process.env.Bucket, MaxKeys: 1000})
-    console.log('listObjects =>', JSON.stringify(files, null, 2))
+    logDebug('listObjects =>', files)
     files.Contents.forEach((file: ExtendedS3Object) => {
       // https://lifegames-app-s3bucket-pq2lluyi2i12.s3.amazonaws.com/20191209-[sxephil].mp4
       file.FileUrl = `https://${files.Name}.s3.amazonaws.com/${encodeURIComponent(file.Key)}`
@@ -162,18 +151,18 @@ export async function listFiles(event: APIGatewayEvent | ScheduledEvent, context
     })
     return response(context, 200, objectKeysToLowerCase(files))
   } catch (error) {
-    console.error(error)
+    logError('response =>', error)
     throw new Error(error)
   }
 }
 
 export async function startFileUpload(event): Promise<UploadPartEvent> {
-  console.log('startFileUpload.event', JSON.stringify(event, null, 2))
+  logInfo('event <=', event)
   try {
     const {fileUrl} = event
-    console.log(`fetchVideoInfo <= ${fileUrl}`)
+    logDebug('fetchVideoInfo <=', fileUrl)
     const myVideoInfo: videoInfo = await fetchVideoInfo(fileUrl)
-    console.log('fetchVideoInfo =>', JSON.stringify(myVideoInfo, null, 2))
+    logDebug('fetchVideoInfo =>', myVideoInfo)
     const myMetadata: Metadata = transformVideoInfoToMetadata(myVideoInfo)
     const myS3File = transformVideoIntoS3File(myVideoInfo, process.env.Bucket)
 
@@ -184,10 +173,10 @@ export async function startFileUpload(event): Promise<UploadPartEvent> {
       url: videoUrl
     }
 
-    console.log('axios <= ', JSON.stringify(options, null, 2))
+    logDebug('axios <= ', options)
     const fileInfo = await axios(options)
     const {status, statusText, headers, config} = fileInfo
-    console.log('axios =>', JSON.stringify({status, statusText, headers, config}, null, 2))
+    logDebug('axios =>', {status, statusText, headers, config})
 
     // TODO: Ensure these headers exist in the response
     const bytesTotal = parseInt(fileInfo.headers['content-length'], 10)
@@ -202,9 +191,9 @@ export async function startFileUpload(event): Promise<UploadPartEvent> {
       Key: key,
       Metadata: myS3File.Metadata
     }
-    console.info('createMultipartUpload <=', JSON.stringify(params, null, 2))
+    logInfo('createMultipartUpload <=', params)
     const output = await createMultipartUpload(params)
-    console.info('createMultipartUpload =>', JSON.stringify(output, null, 2))
+    logInfo('createMultipartUpload =>', output)
     const newPartEnd = Math.min(partSize, bytesTotal)
     return {
       bucket,
@@ -220,13 +209,13 @@ export async function startFileUpload(event): Promise<UploadPartEvent> {
       url: videoUrl
     } as UploadPartEvent
   } catch (error) {
-    console.error(error)
+    logError('response =>', error)
     throw new Error(error)
   }
 }
 
 export async function uploadFilePart(event: UploadPartEvent): Promise<CompleteFileUploadEvent | UploadPartEvent> {
-  console.log('uploadFilePart.event', JSON.stringify(event, null, 2))
+  logInfo('event <=', event)
   try {
     const {bucket, bytesRemaining, bytesTotal, key, partBeg, partEnd, partNumber, partSize, partTags, uploadId, url} = event
     const options: AxiosRequestConfig = {
@@ -236,11 +225,11 @@ export async function uploadFilePart(event: UploadPartEvent): Promise<CompleteFi
       url
     }
 
-    console.info('Requesting ', url)
+    logInfo('axios <=', url)
     const fileInfo = await axios(options)
-    console.log('Fileinfo Data', fileInfo.data)
-    console.log('Fileinfo Status1', fileInfo.status, fileInfo.statusText)
-    console.log('Fileinfo Headers', JSON.stringify(fileInfo.headers, null, 2))
+    logDebug('axios.fileInfo.data =>', fileInfo.data)
+    logDebug('axios.fileInfo.status =>', `${fileInfo.status} ${fileInfo.statusText}`)
+    logDebug('axios.fileInfo.headers =>', fileInfo.headers)
 
     const params: UploadPartRequest = {
       Body: fileInfo.data,
@@ -250,9 +239,9 @@ export async function uploadFilePart(event: UploadPartEvent): Promise<CompleteFi
       PartNumber: partNumber,
       UploadId: uploadId
     }
+    logInfo('uploadPart <=', params)
     const partData = await uploadPart(params)
-    console.log('Completed part', partNumber)
-    console.log('partData', JSON.stringify(partData, null, 2))
+    logInfo('uploadPart =>', partData)
 
     partTags.push({ETag: partData.ETag, PartNumber: partNumber})
     const newPartEnd = Math.min(partEnd + partSize, bytesTotal)
@@ -278,20 +267,20 @@ export async function uploadFilePart(event: UploadPartEvent): Promise<CompleteFi
         partTags,
         uploadId
       } as CompleteFileUploadEvent
-      console.log('Returning finalPart', finalPart)
+      logDebug('response =>', finalPart)
       return finalPart
     } else {
-      console.log('Returning nextPart', nextPart)
+      logDebug('response =>', nextPart)
       return nextPart
     }
   } catch (error) {
-    console.log(error)
+    logError('response =>', error)
     throw new Error(error)
   }
 }
 
 export async function completeFileUpload(event: CompleteFileUploadEvent) {
-  console.log('completeFileUpload.event', JSON.stringify(event, null, 2))
+  logDebug('event', event)
   try {
     const {bucket, key, partTags, uploadId} = event
     const params: CompleteMultipartUploadRequest = {
@@ -300,12 +289,12 @@ export async function completeFileUpload(event: CompleteFileUploadEvent) {
       MultipartUpload: {Parts: partTags},
       UploadId: uploadId
     }
-    console.info('completeMultipartUpload <=', JSON.stringify(params, null, 2))
+    logInfo('completeMultipartUpload <=', params)
     const data = await completeMultipartUpload(params)
-    console.info('completeMultipartUpload =>', JSON.stringify(data, null, 2))
+    logInfo('completeMultipartUpload =>', data)
     return data
   } catch (error) {
-    console.error(error)
+    logError('response =>', error)
     throw new Error(error)
   }
 }
