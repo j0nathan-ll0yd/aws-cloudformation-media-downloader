0a53cba70723c74467723acef826556799319dff
Jonathan Lloyd <jonathan@turo.com>
2020-03-06 19:21:44 -0800

    Added a webhook for S3 event notifications after the file upload completes. Updated the device registration endpoint to also subscribe to an SNS notification topic for push notifications. Added an API Gateway route for logging a client event from a mobile device. Updated some package dependencies and fixtures.

--- local/youtube-dl.js
+++ local/youtube-dl.js
@@ -1,11 +1,11 @@
 var ytdl = require('ytdl-core');
 //var url = 'http://www.youtube.com/watch?v=K1HTMYxjF5Y';
 //var url = 'http://www.youtube.com/watch?v=WKsjaOqDXgg';
-var url = 'https://www.youtube.com/watch?v=-_S81NoYZ3M';
+var url = 'https://www.youtube.com/watch?v=MH0AkoZTBOs';
 var options = {};
 ytdl.getInfo(url, options, function(err, info) {
     if (err) throw err;
-    var format = ytdl.chooseFormat(info.formats, {quality: '22'});
+    // var format = ytdl.chooseFormat(info.formats, {quality: '22'});
     //console.log(JSON.stringify(format));
     console.log(JSON.stringify(info));
 });
--- src/lib/vendor/AWS/SNS.ts
+++ src/lib/vendor/AWS/SNS.ts
@@ -4,7 +4,7 @@ import {
   CreatePlatformEndpointInput, ListEndpointsByPlatformApplicationInput, ListEndpointsByPlatformApplicationResponse,
   ListPlatformApplicationsInput, ListPlatformApplicationsResponse,
   PublishInput,
-  PublishResponse
+  PublishResponse, SubscribeInput, SubscribeResponse
 } from 'aws-sdk/clients/sns'
 import * as AWSXRay from 'aws-xray-sdk'
 const sns = AWSXRay.captureAWSClient(new AWS.SNS({apiVersion: '2010-03-31'}))
@@ -12,6 +12,18 @@ const sns = AWSXRay.captureAWSClient(new AWS.SNS({apiVersion: '2010-03-31'}))
 export function publishSnsEvent(params: PublishInput): Promise<PublishResponse> {
   return new Promise((resolve, reject) => {
     sns.publish(params, (error, data) => {
+      if (error) {
+        reject(error)
+      } else {
+        resolve(data)
+      }
+    })
+  })
+}
+
+export function subscribe(params: SubscribeInput): Promise<SubscribeResponse> {
+  return new Promise((resolve, reject) => {
+    sns.subscribe(params, (error, data) => {
       if (error) {
         reject(error)
       } else {
