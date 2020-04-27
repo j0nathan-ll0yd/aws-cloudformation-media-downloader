74c369186ed8f2cd75f72f3eaa9f4895fa8e7dd4
Jonathan Lloyd <webmaster@lifegames.org>
2020-04-27 12:32:37 -0700

    Added a proxy for getting YouTube information

--- local/youtube-dl.js
+++ local/youtube-dl.js
@@ -1,6 +1,16 @@
 var ytdl = require('ytdl-core');
 var url = 'https://www.youtube.com/watch?v=x8VgBfsRvsc';
-var options = {};
+var HttpsProxyAgent = require('https-proxy-agent');
+var proxy = process.env.http_proxy || 'http://162.223.88.228:8080';
+var agent = new HttpsProxyAgent(proxy);
+var options = {
+    requestOptions: {
+        agent: agent,
+        maxReconnects: 10,
+        maxRetries: 10,
+        backoff: { inc: 100, max: 10000 },
+    }
+};
 ytdl.getInfo(url, options, function(err, info) {
     if (err) throw err;
     // var format = ytdl.chooseFormat(info.formats, {quality: '22'});
