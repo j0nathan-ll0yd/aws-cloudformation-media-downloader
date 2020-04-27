7e69cf1cc41570a9394b22e9a779aa2efda1a9d0
Jonathan Lloyd <webmaster@lifegames.org>
2020-04-27 12:32:37 -0700

    Added a proxy for getting YouTube information

--- src/lib/vendor/YouTube.ts
+++ src/lib/vendor/YouTube.ts
@@ -1,10 +1,16 @@
+import {HttpsProxyAgent} from 'https-proxy-agent'
 import {getInfo, videoInfo, chooseFormat} from 'ytdl-core'
 import {logDebug, logError} from '../../util/lambda-helpers'
 
 export async function fetchVideoInfo(uri): Promise<videoInfo> {
   logDebug('fetchVideoInfo =>')
   try {
-    const info = await getInfo(uri)
+    const info = await getInfo(uri, { requestOptions: {
+      agent: new HttpsProxyAgent('http://162.223.88.228:8080'),
+      maxReconnects: 10,
+      maxRetries: 10,
+      backoff: { inc: 100, max: 10000 }
+    }})
     logDebug('fetchVideoInfo <=')
     return info
   } catch (error) {
