d343b9db5abf6403b699bd8fe2dd98c23346ac1b
Jonathan Lloyd <webmaster@lifegames.org>
2020-07-25 19:42:03 -0700

    Updated deployment scripts to be more readible

--- bin/update-lambda-functions.sh
+++ bin/update-lambda-functions.sh
@@ -3,19 +3,19 @@
 # Get the directory of this file (where the package.json file is located)
 bin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source "${bin_dir}/environment-setup.sh"
-source "${bin_dir}/deployment-setup.sh"
-s3_key_name_dist="dist.zip"
-zip_path="${ARTIFACT_DIR}/${s3_key_name_dist}"
-cd "$bin_dir/../"
-compiled_code_path="dist"
+# setup file paths
+zip_file_path="${bin_dir}/../build/artifacts/dist.zip"
+directory_to_zip="${bin_dir}/../dist"
 
-# make sure typescript will compile
+echo "Running TypeScript checker"
 npm run check-types
-# build compliant JS files in the dist directory
-npm run build
-# zip up the files (exclude OSX dotfiles)
-zip -r -X $zip_path $compiled_code_path
 
+echo "Building distribution directory"
+npm run build
+
+echo "Zipping distribution directory to ${zip_file_path}"
+zip -r -X $zip_file_path $directory_to_zip
+
+echo "Updating Terraform plan"
 cd "${bin_dir}/../terraform"
 terraform apply -auto-approve
--- bin/update-node-modules.sh
+++ bin/update-node-modules.sh
@@ -3,18 +3,11 @@
 # Get the directory of this file (where the package.json file is located)
 bin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-source "${bin_dir}/environment-setup.sh"
-source "${bin_dir}/deployment-setup.sh"
-
-# TODO: Add the command to automatically use only production node_modules
-# npm install --only=prod
-
 cd "$bin_dir/../"
 temp_node_modules="build/nodejs/node_modules"
 node_modules_path="node_modules"
 artifact_dir="${bin_dir}/../build/artifacts"
 artifact_path="${artifact_dir}/layer-node-modules.zip"
-export NODE_MODULES_ARTIFACT_PATH=artifact_path
 
 echo "Moving node_modules directory"
 rm -rf build/nodejs
@@ -26,5 +19,6 @@ echo "Zipping node_modules directory to ${artifact_path}"
 time zip -rqX ${artifact_path} nodejs
 time mv nodejs/node_modules ../node_modules
 
+echo "Updating Terraform plan"
 cd "${bin_dir}/../terraform"
 terraform apply -auto-approve
