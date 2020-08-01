16fea5370835111c32c462093a813e5de1c3b2ba
John Ogle <john@johnogle.info>
2020-08-01 12:50:32 -0700

    Add initial build tools
    Extract the `update-*` scripts into separate `build-*` scripts to work for first time users
    
    Add npm commands `build-code` and `build-node-modules` that call these new scripts
    
    Add instructions in the README file under Getting Started.

--- bin/update-node-modules.sh
+++ bin/update-node-modules.sh
@@ -3,21 +3,7 @@
 # Get the directory of this file (where the package.json file is located)
 bin_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd "$bin_dir/../"
-temp_node_modules="build/nodejs/node_modules"
-node_modules_path="node_modules"
-artifact_dir="${bin_dir}/../build/artifacts"
-artifact_path="${artifact_dir}/layer-node-modules.zip"
-
-echo "Moving node_modules directory"
-rm -rf build/nodejs
-mkdir build/nodejs
-mv node_modules build/nodejs/
-cd "build"
-
-echo "Zipping node_modules directory to ${artifact_path}"
-time zip -rqX ${artifact_path} nodejs
-time mv nodejs/node_modules ../node_modules
+"${bin_dir}/build-node-modules.sh"
 
 echo "Updating Terraform plan"
 cd "${bin_dir}/../terraform"
