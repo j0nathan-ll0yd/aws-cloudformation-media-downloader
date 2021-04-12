e72928d58ce6e7376456941ba8489b4b52d177c0
Jonathan Lloyd <webmaster@lifegames.org>
2021-04-11 17:28:28 -0700

    task: Removed unused imports

--- src/lib/vendor/AWS/SNS.ts
+++ src/lib/vendor/AWS/SNS.ts
@@ -1,6 +1,5 @@
 import * as AWS from 'aws-sdk'
 import {
-  ConfirmSubscriptionInput, ConfirmSubscriptionResponse,
   CreateEndpointResponse,
   CreatePlatformEndpointInput, ListSubscriptionsByTopicInput, ListSubscriptionsByTopicResponse,
   PublishInput,
