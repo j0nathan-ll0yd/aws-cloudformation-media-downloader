c03db0965a39281aeb12ffb33ad5bea57fa54f92
Jonathan Lloyd <341230+j0nathan-ll0yd@users.noreply.github.com>
2024-08-17 20:54:52 -0700

    Corrected DynamoDB expressions

--- src/lib/vendor/AWS/DynamoDB.ts
+++ src/lib/vendor/AWS/DynamoDB.ts
@@ -1,28 +1,28 @@
-import {DynamoDBClient, DeleteItemInput, DeleteItemOutput, QueryInput, QueryOutput, UpdateItemInput, UpdateItemOutput, PutItemInput, PutItemOutput, ScanInput, ScanOutput, BatchGetItemInput, BatchGetItemOutput} from '@aws-sdk/client-dynamodb'
+import {BatchGetItemInput, DeleteItemInput, DynamoDBClient, PutItemInput, QueryInput, ScanInput, UpdateItemInput} from '@aws-sdk/client-dynamodb'
 import {DynamoDBDocument} from '@aws-sdk/lib-dynamodb'
 const client = new DynamoDBClient()
 const docClient = DynamoDBDocument.from(client)
 
-export function updateItem(params: UpdateItemInput): Promise<UpdateItemOutput> {
+export function updateItem(params: UpdateItemInput) {
   return docClient.update(params)
 }
 
-export function putItem(params: PutItemInput): Promise<PutItemOutput> {
+export function putItem(params: PutItemInput) {
   return docClient.put(params)
 }
 
-export function scan(params: ScanInput): Promise<ScanOutput> {
+export function scan(params: ScanInput) {
   return docClient.scan(params)
 }
 
-export function batchGet(params: BatchGetItemInput): Promise<BatchGetItemOutput> {
+export function batchGet(params: BatchGetItemInput) {
   return docClient.batchGet(params)
 }
 
-export function query(params: QueryInput): Promise<QueryOutput> {
+export function query(params: QueryInput) {
   return docClient.query(params)
 }
 
-export function deleteItem(params: DeleteItemInput): Promise<DeleteItemOutput> {
+export function deleteItem(params: DeleteItemInput) {
   return docClient.delete(params)
 }
--- src/util/dynamodb-helpers.ts
+++ src/util/dynamodb-helpers.ts
@@ -1,7 +1,6 @@
 import {DynamoDBFile, IdentityProviderApple, User, Device} from '../types/main'
 import {FileStatus} from '../types/enums'
-import {DeleteItemInput, QueryInput, UpdateItemInput, PutItemInput, ScanInput, BatchGetItemInput} from '@aws-sdk/client-dynamodb'
-import {marshall} from '@aws-sdk/util-dynamodb'
+import {BatchGetItemInput, DeleteItemInput, PutItemInput, QueryInput, ScanInput, UpdateItemInput} from '@aws-sdk/client-dynamodb'
 
 function transformObjectToDynamoUpdateQuery(item: object) {
   let UpdateExpression = 'SET'
@@ -28,225 +27,225 @@ function transformObjectToDynamoUpdateQuery(item: object) {
   }
 }
 
-export function updateCompletedFileParams(tableName: string, fileId: string, fileUrl: string): UpdateItemInput {
+export function updateCompletedFileParams(tableName: string, fileId: string, fileUrl: string) {
   return {
     ExpressionAttributeNames: {'#FN': 'url', '#S': 'status'},
-    ExpressionAttributeValues: marshall({':fn': fileUrl, ':s': FileStatus.Downloaded}),
-    Key: marshall({fileId: fileId}),
+    ExpressionAttributeValues: {':fn': fileUrl, ':s': FileStatus.Downloaded},
+    Key: {fileId: fileId},
     ReturnValues: 'ALL_NEW',
     TableName: tableName,
     UpdateExpression: 'SET #FN = :fn, #S = :s'
-  } as UpdateItemInput
+  } as unknown as UpdateItemInput
 }
 
-export function scanForFileParams(tableName: string): ScanInput {
+export function scanForFileParams(tableName: string) {
   return {
     ExpressionAttributeNames: {
       '#AA': 'availableAt',
       '#FID': 'fileId',
       '#FN': 'url'
     },
-    ExpressionAttributeValues: marshall({
+    ExpressionAttributeValues: {
       ':aa': parseInt(Date.now().toString(), 10)
-    }),
+    },
     FilterExpression: '#AA <= :aa AND attribute_not_exists(#FN)',
     ProjectionExpression: '#AA, #FID',
     TableName: tableName
-  } as ScanInput
+  } as unknown as ScanInput
 }
 
-export function getFileByKey(tableName: string, fileName: string): ScanInput {
+export function getFileByKey(tableName: string, fileName: string) {
   return {
     ExpressionAttributeNames: {'#key': 'key'},
-    ExpressionAttributeValues: marshall({':key': fileName}),
+    ExpressionAttributeValues: {':key': fileName},
     FilterExpression: '#key = :key',
     TableName: tableName
-  } as ScanInput
+  } as unknown as ScanInput
 }
 
-export function getUsersByFileId(tableName: string, fileId: string): ScanInput {
+export function getUsersByFileId(tableName: string, fileId: string) {
   return {
-    ExpressionAttributeValues: marshall({':fileId': fileId}),
+    ExpressionAttributeValues: {':fileId': fileId},
     FilterExpression: 'contains (fileId, :fileId)',
     TableName: tableName
-  } as ScanInput
+  } as unknown as ScanInput
 }
 
-export function getUsersByDeviceId(tableName: string, deviceId: string): ScanInput {
+export function getUsersByDeviceId(tableName: string, deviceId: string) {
   return {
-    ExpressionAttributeValues: marshall({':deviceId': deviceId}),
+    ExpressionAttributeValues: {':deviceId': deviceId},
     FilterExpression: 'contains (devices, :deviceId)',
     TableName: tableName
-  } as ScanInput
+  } as unknown as ScanInput
 }
 
-export function userFileParams(tableName: string, userId: string, fileId: string): UpdateItemInput {
+export function userFileParams(tableName: string, userId: string, fileId: string) {
   return {
     ExpressionAttributeNames: {'#FID': 'fileId'},
-    ExpressionAttributeValues: marshall({':fid': new Set([fileId])}),
-    Key: marshall({userId: userId}),
-    ReturnValues: 'NONE',
+    ExpressionAttributeValues: {':fid': new Set([fileId])},
+    Key: {userId: userId},
+    ReturnValues: 'ALL_NEW',
     UpdateExpression: 'ADD #FID :fid',
     TableName: tableName
-  } as UpdateItemInput
+  } as unknown as UpdateItemInput
 }
 
-export function userDevicesParams(tableName: string, userId: string, deviceId: string): UpdateItemInput {
+export function userDevicesParams(tableName: string, userId: string, deviceId: string) {
   return {
     ExpressionAttributeNames: {'#DID': 'devices'},
-    ExpressionAttributeValues: marshall({':did': new Set([deviceId])}),
-    Key: marshall({userId: userId}),
+    ExpressionAttributeValues: {':did': new Set([deviceId])},
+    Key: {userId: userId},
     ReturnValues: 'NONE',
     UpdateExpression: 'ADD #DID :did',
     TableName: tableName
-  } as UpdateItemInput
+  } as unknown as UpdateItemInput
 }
 
-export function deleteSingleUserDeviceParams(tableName: string, userId: string, deviceId: string): UpdateItemInput {
+export function deleteSingleUserDeviceParams(tableName: string, userId: string, deviceId: string) {
   return {
     TableName: tableName,
-    Key: marshall({userId}),
+    Key: {userId},
     UpdateExpression: 'DELETE devices :deviceId',
-    ExpressionAttributeValues: marshall({':deviceId': new Set([deviceId])})
-  } as UpdateItemInput
+    ExpressionAttributeValues: {':deviceId': new Set([deviceId])}
+  } as unknown as UpdateItemInput
 }
 
-export function upsertDeviceParams(tableName: string, device: Device): UpdateItemInput {
+export function upsertDeviceParams(tableName: string, device: Device) {
   const {deviceId, ...deviceSubset} = device
   const {UpdateExpression, ExpressionAttributeNames, ExpressionAttributeValues} = transformObjectToDynamoUpdateQuery(deviceSubset)
   return {
-    Key: marshall({deviceId}),
+    Key: {deviceId},
     TableName: tableName,
     UpdateExpression,
     ExpressionAttributeNames,
-    ExpressionAttributeValues: marshall(ExpressionAttributeValues)
-  } as UpdateItemInput
+    ExpressionAttributeValues
+  } as unknown as UpdateItemInput
 }
 
-export function queryUserDeviceParams(tableName: string, userId: string): QueryInput {
+export function queryUserDeviceParams(tableName: string, userId: string) {
   return {
     TableName: tableName,
     KeyConditionExpression: 'userId = :userId',
-    ExpressionAttributeValues: marshall({':userId': userId})
-  } as QueryInput
+    ExpressionAttributeValues: {':userId': userId}
+  } as unknown as QueryInput
 }
 
-export function queryFileParams(tableName: string, fileId: string): QueryInput {
+export function queryFileParams(tableName: string, fileId: string) {
   return {
     TableName: tableName,
     KeyConditionExpression: 'fileId = :fileId',
-    ExpressionAttributeValues: marshall({':fileId': fileId})
-  } as QueryInput
+    ExpressionAttributeValues: {':fileId': fileId}
+  } as unknown as QueryInput
 }
 
-export function queryDeviceParams(tableName: string, deviceId: string): QueryInput {
+export function queryDeviceParams(tableName: string, deviceId: string) {
   return {
     TableName: tableName,
     KeyConditionExpression: 'deviceId = :deviceId',
-    ExpressionAttributeValues: marshall({':deviceId': deviceId})
-  } as QueryInput
+    ExpressionAttributeValues: {':deviceId': deviceId}
+  } as unknown as QueryInput
 }
 
-export function getUserDeviceByUserIdParams(tableName: string, userId: string): QueryInput {
+export function getUserDeviceByUserIdParams(tableName: string, userId: string) {
   return {
     TableName: tableName,
     KeyConditionExpression: 'userId = :userId',
-    ExpressionAttributeValues: marshall({':userId': userId})
-  } as QueryInput
+    ExpressionAttributeValues: {':userId': userId}
+  } as unknown as QueryInput
 }
 
-export function getDeviceParams(tableName: string, deviceId: string): QueryInput {
+export function getDeviceParams(tableName: string, deviceId: string) {
   return {
     TableName: tableName,
     KeyConditionExpression: 'deviceId = :deviceId',
-    ExpressionAttributeValues: marshall({':deviceId': deviceId})
-  } as QueryInput
+    ExpressionAttributeValues: {':deviceId': deviceId}
+  } as unknown as QueryInput
 }
 
-export function deleteDeviceParams(tableName: string, deviceId: string): DeleteItemInput {
+export function deleteDeviceParams(tableName: string, deviceId: string) {
   return {
     TableName: tableName,
-    Key: marshall({deviceId})
-  } as DeleteItemInput
+    Key: {deviceId}
+  } as unknown as DeleteItemInput
 }
 
-export function deleteUserParams(tableName: string, userId: string): DeleteItemInput {
+export function deleteUserParams(tableName: string, userId: string) {
   return {
     TableName: tableName,
-    Key: marshall({userId})
-  } as DeleteItemInput
+    Key: {userId}
+  } as unknown as DeleteItemInput
 }
 
-export function deleteUserFilesParams(tableName: string, userId: string): DeleteItemInput {
+export function deleteUserFilesParams(tableName: string, userId: string) {
   return {
     TableName: tableName,
-    Key: marshall({userId})
-  } as DeleteItemInput
+    Key: {userId}
+  } as unknown as DeleteItemInput
 }
 
-export function deleteAllUserDeviceParams(tableName: string, userId: string): DeleteItemInput {
+export function deleteAllUserDeviceParams(tableName: string, userId: string) {
   return {
     TableName: tableName,
-    Key: marshall({userId})
-  } as DeleteItemInput
+    Key: {userId}
+  } as unknown as DeleteItemInput
 }
 
-export function newFileParams(tableName: string, fileId: string): UpdateItemInput {
+export function newFileParams(tableName: string, fileId: string) {
   return {
     ExpressionAttributeNames: {
       '#AA': 'availableAt',
       '#S': 'status'
     },
-    ExpressionAttributeValues: marshall({
+    ExpressionAttributeValues: {
       ':aa': parseInt(Date.now().toString(), 10),
       ':s': FileStatus.PendingMetadata
-    }),
-    Key: marshall({fileId: fileId}),
+    },
+    Key: {fileId: fileId},
     ReturnValues: 'ALL_OLD',
     UpdateExpression: 'SET #AA = :aa, #S = :s',
     TableName: tableName
-  } as UpdateItemInput
+  } as unknown as UpdateItemInput
 }
 
-export function newUserParams(tableName: string, user: User, identityProviderApple: IdentityProviderApple): PutItemInput {
+export function newUserParams(tableName: string, user: User, identityProviderApple: IdentityProviderApple) {
   return {
-    Item: marshall({
+    Item: {
       ...user,
       identityProviders: {...identityProviderApple}
-    }),
+    },
     TableName: tableName
-  } as PutItemInput
+  } as unknown as PutItemInput
 }
 
-export function updateFileMetadataParams(tableName: string, item: DynamoDBFile): UpdateItemInput {
+export function updateFileMetadataParams(tableName: string, item: DynamoDBFile) {
   const {UpdateExpression, ExpressionAttributeNames, ExpressionAttributeValues} = transformObjectToDynamoUpdateQuery(item)
   return {
-    Key: marshall({fileId: item.fileId}),
+    Key: {fileId: item.fileId},
     TableName: tableName,
     UpdateExpression,
     ExpressionAttributeNames,
-    ExpressionAttributeValues: marshall(ExpressionAttributeValues)
-  } as UpdateItemInput
+    ExpressionAttributeValues
+  } as unknown as UpdateItemInput
 }
 
-export function getUserFilesParams(tableName: string, userId: string): QueryInput {
+export function getUserFilesParams(tableName: string, userId: string) {
   return {
-    ExpressionAttributeValues: marshall({
+    ExpressionAttributeValues: {
       ':uid': userId
-    }),
+    },
     ExpressionAttributeNames: {
       '#uid': 'userId'
     },
     KeyConditionExpression: '#uid = :uid',
     TableName: tableName
-  } as QueryInput
+  } as unknown as UpdateItemInput
 }
 
-export function getBatchFilesParams(tableName: string, files: string[]): BatchGetItemInput {
+export function getBatchFilesParams(tableName: string, files: string[]) {
   const Keys = []
   const mySet = new Set(files)
   for (const fileId of mySet.values()) {
-    Keys.push(marshall({fileId}))
+    Keys.push({fileId})
   }
 
   return {
@@ -255,13 +254,13 @@ export function getBatchFilesParams(tableName: string, files: string[]): BatchGe
         Keys
       }
     }
-  } as BatchGetItemInput
+  } as unknown as BatchGetItemInput
 }
 
-export function getUserByAppleDeviceIdentifierParams(tableName: string, userId: string): ScanInput {
+export function getUserByAppleDeviceIdentifierParams(tableName: string, userId: string) {
   return {
-    ExpressionAttributeValues: marshall({':userId': userId}),
+    ExpressionAttributeValues: {':userId': userId},
     FilterExpression: 'identityProviders.userId = :userId',
     TableName: tableName
-  } as ScanInput
+  } as unknown as ScanInput
 }
